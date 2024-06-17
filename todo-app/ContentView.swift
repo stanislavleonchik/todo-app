743804fc57690f48558b315d02cd0db5307ca93e
Stanislav Leonchik <leonstas1@mail.ru>
2024-06-18 01:07:53 +0300

    Add TodoItem

--- todo-app/ContentView.swift
+++ todo-app/ContentView.swift
@@ -5,6 +5,33 @@
 //  Created by Stanislav Leonchik on 18.06.2024.
 //
 
+struct TodoItem {
+    enum Importance { case unimportant, oridnary, important }
+    let id: String
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isDone: Bool
+    let dateCreated: Date
+    let dateChanged: Date?
+    
+    init(id: String = UUID().uuidString,
+         text: String,
+         importance: Importance = .oridnary,
+         deadline: Date?,
+         isDone: Bool,
+         dateCreated: Date = Date(),
+         dateChanged: Date?) {
+        self.id = id
+        self.text = text
+        self.importance = importance
+        self.deadline = deadline
+        self.isDone = isDone
+        self.dateCreated = dateCreated
+        self.dateChanged = dateChanged
+    }
+}
+
 import SwiftUI
 
 struct ContentView: View {
